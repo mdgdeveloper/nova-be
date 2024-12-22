babaa811618a8f6adb923dcfd69a8d6ef5e11a69
smadrigalcef <smadrigal@unicef.org>
2024-12-22 21:47:04 +0100

    0.1.4: API resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ClientsModule } from './clients/clients.module';
+import { EmployeesModule } from './employees/employees.module';
+import { ProductsModule } from './products/products.module';
+import { ServicesModule } from './services/services.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule, EmployeesModule, ProductsModule, ServicesModule, TransactionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
